79b0040a34f1bf85bd014d7e97530e5499db6793
Tirtha Ghosh <tirtha15>
2016-06-18 19:18:15 -0700

    game and user model

---
+++ api/models/Game.js
@@ -0,0 +1,42 @@
+/**
+ * Game.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id:{
+      type: 'string',
+      size: 36,
+      primaryKey: true,
+      unique: true,
+    },
+    rows: 'integer',
+    columns: 'integer',
+    maxPlayers: 'integer',
+    minPlayers: 'integer',
+    blockTime: 'integer',
+    players: {
+      collection: 'user',
+      via: 'games',
+    },
+    startTime: 'dateTime'
+  },
+  beforeCreate: function(values, next) {
+    if (values.id) {
+      delete values.id;
+    }
+    if (values.createdAt) {
+      delete values.createdAt;
+    }
+    if (values.updatedAt) {
+      delete values.updatedAt;
+    }
+    values.id = utils.uuid();
+    next();
+  },
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,41 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id:{
+      type: 'string',
+      size: 36,
+      primaryKey: true,
+      unique: true,
+    },
+    name: {
+	  type: 'string',
+	  required: true
+    },
+    ip: 'string',
+    games: {
+      collection: 'game',
+      via: 'players',
+    },
+  },
+  beforeCreate: function(values, next) {
+    if (values.id) {
+      delete values.id;
+    }
+    if (values.createdAt) {
+      delete values.createdAt;
+    }
+    if (values.updatedAt) {
+      delete values.updatedAt;
+    }
+    values.id = utils.uuid();
+    next();
+  },
+};
+
